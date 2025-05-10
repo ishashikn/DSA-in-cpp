d7608fa998fbcc792a6bb5e5a877565c4a1cc32e
Shashi Kumar N <94559295+ishashikn@users.noreply.github.com>
2025-05-10 12:51:55 +0530

    4.rotate_array_d.cpp

--- Arrays/4.rotate_array_d.cpp
+++ Arrays/4.rotate_array_d.cpp
@@ -44,7 +44,7 @@ int main() {
     int n = arr.size();
     d = d % n;
     
-    reverse(arr, 0, d-1);
+    reverse(arr, 0, d-1);        
     reverse(arr, d, n-1);
     reverse(arr, 0 , n-1);
     
